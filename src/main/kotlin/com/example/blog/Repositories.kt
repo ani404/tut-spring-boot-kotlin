f40cb8d918b410fa8fa1f36c8909a712e03198f5
ani404 <chhotu.aniket@moneyforward.co.jp>
2023-12-22 10:44:36 +0900

    code_refactor Repositories.kt

--- src/main/kotlin/com/example/blog/Repositories.kt
+++ src/main/kotlin/com/example/blog/Repositories.kt
@@ -2,12 +2,17 @@ package com.example.blog
 
 import org.springframework.data.repository.CrudRepository
 
+// Repository interface for handling Article entities
 interface ArticleRepository : CrudRepository<Article, Long> {
+	// Find an article by its slug
 	fun findBySlug(slug: String): Article?
+
+	// Find all articles ordered by added date in descending order
 	fun findAllByOrderByAddedAtDesc(): Iterable<Article>
 }
 
+// Repository interface for handling User entities
 interface UserRepository : CrudRepository<User, Long> {
+	// Find a user by their login
 	fun findByLogin(login: String): User?
 }
-
