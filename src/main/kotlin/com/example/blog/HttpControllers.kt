3c6704b0db3220e11441110c5c0915a207a40854
ani404 <chhotu.aniket@moneyforward.co.jp>
2023-12-22 10:44:26 +0900

    code_refactor HttpControllers.kt

--- src/main/kotlin/com/example/blog/HttpControllers.kt
+++ src/main/kotlin/com/example/blog/HttpControllers.kt
@@ -4,26 +4,32 @@ import org.springframework.http.HttpStatus.*
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 
+// Controller for managing articles via API
 @RestController
 @RequestMapping("/api/article")
 class ArticleController(private val repository: ArticleRepository) {
 
+	// Retrieve all articles and return them sorted by added date in descending order
 	@GetMapping("/")
 	fun findAll() = repository.findAllByOrderByAddedAtDesc()
 
+	// Retrieve a specific article by slug
 	@GetMapping("/{slug}")
 	fun findOne(@PathVariable slug: String) =
-			repository.findBySlug(slug) ?: throw ResponseStatusException(NOT_FOUND, "This article does not exist")
-
+		repository.findBySlug(slug) ?: throw ResponseStatusException(NOT_FOUND, "This article does not exist")
 }
 
+// Controller for managing users via API
 @RestController
 @RequestMapping("/api/user")
 class UserController(private val repository: UserRepository) {
 
+	// Retrieve all users
 	@GetMapping("/")
 	fun findAll(): MutableIterable<User> = repository.findAll()
 
+	// Retrieve a specific user by login
 	@GetMapping("/{login}")
-	fun findOne(@PathVariable login: String) = repository.findByLogin(login) ?: throw ResponseStatusException(NOT_FOUND, "This user does not exist")
+	fun findOne(@PathVariable login: String) =
+		repository.findByLogin(login) ?: throw ResponseStatusException(NOT_FOUND, "This user does not exist")
 }
