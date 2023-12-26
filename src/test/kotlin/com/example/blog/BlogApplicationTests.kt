f4230e5f307e1b2c549b2c42411a51f05d92d89c
ani404 <chhotu.aniket@moneyforward.co.jp>
2023-12-26 17:46:53 +0900

    Added more BlogApplicationTests

--- src/test/kotlin/com/example/blog/BlogApplicationTests.kt
+++ src/test/kotlin/com/example/blog/BlogApplicationTests.kt
@@ -1,13 +1,35 @@
 package com.example.blog
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class BlogApplicationTests {
 
+	@Autowired
+	lateinit var userRepository: UserRepository
+
+	@Autowired
+	lateinit var articleRepository: ArticleRepository
+
 	@Test
-	fun contextLoads() {
+	fun `Context loads`() {
+		// Verifies that the context loads successfully
 	}
 
+	@Test
+	fun `UserRepository should be accessible`() {
+		// Verifies that UserRepository is accessible and not null
+		assert(userRepository != null)
+	}
+
+	@Test
+	fun `ArticleRepository should be accessible`() {
+		// Verifies that ArticleRepository is accessible and not null
+		assert(articleRepository != null)
+	}
+
+	// Add more test cases to cover specific functionalities or interactions
 }
