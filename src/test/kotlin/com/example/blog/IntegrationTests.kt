675ad4ca9b70c81524f700313e2eef6ec92a8ca4
ani404 <chhotu.aniket@moneyforward.co.jp>
2023-12-26 17:41:07 +0900

    Added more IntegrationTests

--- src/test/kotlin/com/example/blog/IntegrationTests.kt
+++ src/test/kotlin/com/example/blog/IntegrationTests.kt
@@ -35,9 +35,47 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 		assertThat(entity.body).contains(title, "Lorem", "dolor sit amet")
 	}
 
+	// Additional tests with different data
+	@Test
+	fun `Assert another article page title, content and status code`() {
+		println(">> Assert another article page title, content and status code")
+		val title = "Another Article"
+		val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
+		assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+		assertThat(entity.body).contains(title, "Another Article Content", "Additional data")
+	}
+
+	// Additional tests with different data
+	@Test
+	fun `Assert third article page title, content and status code`() {
+		println(">> Assert third article page title, content and status code")
+		val title = "Third Article"
+		val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
+		assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+		assertThat(entity.body).contains(title, "Third Article Content", "Additional data")
+	}
+
+	// Adding more similar tests
+	@Test
+	fun `Assert fourth article page title, content and status code`() {
+		println(">> Assert fourth article page title, content and status code")
+		val title = "Fourth Article"
+		val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
+		assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+		assertThat(entity.body).contains(title, "Fourth Article Content", "Additional data")
+	}
+
+	@Test
+	fun `Assert fifth article page title, content and status code`() {
+		println(">> Assert fifth article page title, content and status code")
+		val title = "Fifth Article"
+		val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
+		assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+		assertThat(entity.body).contains(title, "Fifth Article Content", "Additional data")
+	}
+
 	@AfterAll
 	fun teardown() {
 		println(">> Tear down")
 	}
-
 }
